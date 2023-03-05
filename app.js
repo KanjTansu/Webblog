c17f2c7c79d6fd93bde1ee33d0c3c6beece30889
Kanj Tansuwongs <kanjtansu@gmail.com>
2023-03-05 09:32:39 +0700

    Edit port to run on Render server

--- app.js
+++ app.js
@@ -108,6 +108,13 @@ app.post('/compose', async (req, res) => {
 })
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function () {
+//   console.log("Server started on port 3000");
+// });
+let port = process.env.PORT;
+if (port == "null" || port =="" || port == "undefined") {
+  port =3000;
+}
+app.listen(port, function () {
+  console.log("Server running on port " + port);
+})
